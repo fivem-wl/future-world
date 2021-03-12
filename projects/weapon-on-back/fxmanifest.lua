ab10f4b02bc48b566d9cd563c141303b7eccc5e2
imckl <imckl@outlook.com>
2021-03-13 00:22:24 +0800

    update: weapon-on-back - fxmanifest.lua

--- projects/weapon-on-back/fxmanifest.lua
+++ projects/weapon-on-back/fxmanifest.lua
@@ -3,14 +3,14 @@ game 'gta5'
 
 name 'weapon-on-back'
 description 'Put/Sheath weapons on back, with components and tint'
-author 'imckl'
+author 'imckl <imckl@outlook.com>'
 url 'https://github.com/fivem-wl/future-world'
 
 client_script '*.client.js'
 server_script '*.server.js'
 
---  local interval in milliseconds
+--  local interval in milliseconds, increase it if encountering performance issue
 local_interval_ms '200'
 
---  network operation interval in milliseconds, increase it if bandwidth usage is too high
+--  network interval in milliseconds, increase it if encountering bandwidth issue
 network_interval_ms '1000'
